cba750b930497a5340e955b314b8058b8213ff4e
SAMUEL SILVA <samuel.brazz@gmail.com>
2024-06-15 11:32:44 -0300

    Commit inicial

---
+++ app/src/main/java/app/lava_jato_pipdm/MainActivity.java
@@ -0,0 +1,14 @@
+package app.lava_jato_pipdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lava_jato-PIPDM"
+include(":app")
