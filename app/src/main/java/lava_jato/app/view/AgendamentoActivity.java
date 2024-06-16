6c3e259d82907855fc30118659c0802cca4bcf46
macostacurta <mamcostacurta@gmail.com>
2024-06-16 20:12:18 -0300

    lalalalalalalala

--- app/src/main/java/lava_jato/app/view/AgendamentoActivity.java
+++ app/src/main/java/lava_jato/app/view/AgendamentoActivity.java
@@ -2,18 +2,16 @@ package lava_jato.app.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import app.lava_jato_pipdm.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class AgendamentoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_agendamento);
 
     }
 
